04b6d56706ebb07ac0faf3df940b557b330d75fe
nickc01 <nickc01@outlook.com>
2023-05-30 17:22:42 -0500

    Added Initial Files

---
+++ Assets/MoreGodhomeSpaceMod.cs
@@ -0,0 +1,9 @@
+﻿using WeaverCore;
+
+public class MoreGodhomeSpaceMod : WeaverMod
+{
+    public override string GetVersion()
+    {
+        return "1.0.0.0";
+    }
+}
---
+++ Assets/OnSceneStart.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using WeaverCore;
+using WeaverCore.Attributes;
+using WeaverCore.Utilities;
+
+public static class OnSceneStart
+{
+    static List<string> deletedObjects = new List<string>
+    {
+        "Chunk 2 0",
+        "Chunk 2 1",
+        "Chunk 2 2",
+        "Chunk 2 3",
+        "Chunk 2 4",
+        "GG_scenery_0001_1 (21)",
+        "Chunk 1 0",
+        "Chunk 2 5",
+        "Roof Collider (3)"
+    };
+
+
+    [OnRuntimeInit]
+    static void OnGameStart()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    }
+
+    static void DeleteObjects(GameObject obj)
+    {
+        if (deletedObjects.Contains(obj.name))
+        {
+            GameObject.Destroy(obj);
+        }
+
+        if (obj.transform.childCount > 0)
+        {
+            for (int i = obj.transform.childCount - 1; i >= 0; i--)
+            {
+                DeleteObjects(obj.transform.GetChild(i).gameObject);
+            }
+        }
+    }
+
+    private static void SceneManager_sceneLoaded(UnityEngine.SceneManagement.Scene arg0, UnityEngine.SceneManagement.LoadSceneMode arg1)
+    {
+        if (arg0.name == "GG_Workshop")
+        {
+            foreach (var obj in arg0.GetRootGameObjects())
+            {
+                DeleteObjects(obj);
+                if (obj.name == "BlurPlane")
+                {
+                    obj.transform.SetYLocalPosition(57.5149f);
+                    obj.transform.SetYLocalScale(12.82298f);
+                }
+                else if (obj.name == "water_fog (1)")
+                {
+                    obj.transform.SetPositionX(130.2658f);
+                    obj.transform.SetScaleX(319.3503f);
+                }
+                else if (obj.name == "water_fog (2)")
+                {
+                    obj.transform.SetPositionX(129.9462f);
+                    obj.transform.SetScaleX(320.3779f);
+                }
+            }
+        }
+
+        var tilemapType = typeof(HeroController).Assembly.GetType("tk2dTileMap");
+
+        WeaverLog.Log("TILEMAP TYPE = " + tilemapType.FullName);
+
+        if (tilemapType != null)
+        {
+            foreach (var obj in arg0.GetRootGameObjects())
+            {
+                if (obj.TryGetComponent(tilemapType, out var component))
+                {
+                    //var widthField = tilemapType.GetField("width");
+                    var heightField = tilemapType.GetField("height");
+
+                    heightField.SetValue(component, 93);
+                    break;
+                }
+            }
+        }
+    }
+}
