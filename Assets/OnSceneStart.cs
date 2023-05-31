46d7f580de940964e327a64c24c89580859ec1de
nickc01 <nickc01@outlook.com>
2023-05-31 12:14:12 -0500

    Added Try-Catch to Scene Loaded Hook

--- Assets/OnSceneStart.cs
+++ Assets/OnSceneStart.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,85 +7,94 @@ using WeaverCore;
 using WeaverCore.Attributes;
 using WeaverCore.Utilities;
 
-public static class OnSceneStart
+namespace MoreGodhomeSpace
 {
-    static List<string> deletedObjects = new List<string>
+    public static class OnSceneStart
     {
-        "Chunk 2 0",
-        "Chunk 2 1",
-        "Chunk 2 2",
-        "Chunk 2 3",
-        "Chunk 2 4",
-        "GG_scenery_0001_1 (21)",
-        "Chunk 1 0",
-        "Chunk 2 5",
-        "Roof Collider (3)"
-    };
-
-
-    [OnRuntimeInit]
-    static void OnGameStart()
-    {
-        UnityEngine.SceneManagement.SceneManager.sceneLoaded += SceneManager_sceneLoaded;
-    }
-
-    static void DeleteObjects(GameObject obj)
-    {
-        if (deletedObjects.Contains(obj.name))
+        static List<string> deletedObjects = new List<string>
         {
-            GameObject.Destroy(obj);
+            "Chunk 2 0",
+            "Chunk 2 1",
+            "Chunk 2 2",
+            "Chunk 2 3",
+            "Chunk 2 4",
+            "GG_scenery_0001_1 (21)",
+            "Chunk 1 0",
+            "Chunk 2 5",
+            "Roof Collider (3)"
+        };
+
+
+        [OnRuntimeInit]
+        static void OnGameStart()
+        {
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += SceneManager_sceneLoaded;
         }
 
-        if (obj.transform.childCount > 0)
+        static void DeleteObjects(GameObject obj)
         {
-            for (int i = obj.transform.childCount - 1; i >= 0; i--)
+            if (deletedObjects.Contains(obj.name))
             {
-                DeleteObjects(obj.transform.GetChild(i).gameObject);
+                GameObject.Destroy(obj);
             }
-        }
-    }
 
-    private static void SceneManager_sceneLoaded(UnityEngine.SceneManagement.Scene arg0, UnityEngine.SceneManagement.LoadSceneMode arg1)
-    {
-        if (arg0.name == "GG_Workshop")
-        {
-            foreach (var obj in arg0.GetRootGameObjects())
+            if (obj.transform.childCount > 0)
             {
-                DeleteObjects(obj);
-                if (obj.name == "BlurPlane")
+                for (int i = obj.transform.childCount - 1; i >= 0; i--)
                 {
-                    obj.transform.SetYLocalPosition(57.5149f);
-                    obj.transform.SetYLocalScale(12.82298f);
-                }
-                else if (obj.name == "water_fog (1)")
-                {
-                    obj.transform.SetPositionX(130.2658f);
-                    obj.transform.SetScaleX(319.3503f);
-                }
-                else if (obj.name == "water_fog (2)")
-                {
-                    obj.transform.SetPositionX(129.9462f);
-                    obj.transform.SetScaleX(320.3779f);
+                    DeleteObjects(obj.transform.GetChild(i).gameObject);
                 }
             }
         }
 
-        var tilemapType = typeof(HeroController).Assembly.GetType("tk2dTileMap");
-
-        WeaverLog.Log("TILEMAP TYPE = " + tilemapType.FullName);
-
-        if (tilemapType != null)
+        private static void SceneManager_sceneLoaded(UnityEngine.SceneManagement.Scene arg0, UnityEngine.SceneManagement.LoadSceneMode arg1)
         {
-            foreach (var obj in arg0.GetRootGameObjects())
+            try
             {
-                if (obj.TryGetComponent(tilemapType, out var component))
+                if (arg0.name == "GG_Workshop")
                 {
-                    //var widthField = tilemapType.GetField("width");
-                    var heightField = tilemapType.GetField("height");
-
-                    heightField.SetValue(component, 93);
-                    break;
+                    foreach (var obj in arg0.GetRootGameObjects())
+                    {
+                        DeleteObjects(obj);
+                        if (obj.name == "BlurPlane")
+                        {
+                            obj.transform.SetYLocalPosition(57.5149f);
+                            obj.transform.SetYLocalScale(12.82298f);
+                        }
+                        else if (obj.name == "water_fog (1)")
+                        {
+                            obj.transform.SetPositionX(130.2658f);
+                            obj.transform.SetScaleX(319.3503f);
+                        }
+                        else if (obj.name == "water_fog (2)")
+                        {
+                            obj.transform.SetPositionX(129.9462f);
+                            obj.transform.SetScaleX(320.3779f);
+                        }
+                    }
                 }
+
+                var tilemapType = typeof(HeroController).Assembly.GetType("tk2dTileMap");
+
+                if (tilemapType != null)
+                {
+                    foreach (var obj in arg0.GetRootGameObjects())
+                    {
+                        if (obj.TryGetComponent(tilemapType, out var component))
+                        {
+                            //var widthField = tilemapType.GetField("width");
+                            var heightField = tilemapType.GetField("height");
+
+                            heightField.SetValue(component, 93);
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Something failed when trying to replace scene objects. May or may not be a big problem");
+                Debug.LogException(e);
             }
         }
     }
